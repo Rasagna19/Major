42e38005ad6e5da22fe4fc78410de59243e99902
Rasagna19 <113854246+Rasagna19@users.noreply.github.com>
2022-09-19 00:11:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import joblib
-model_nb = joblib.load('NEWS')
+model_nb = joblib.load('news')
 st.title('NEWS CLASSIFIER') #creates a title in web app
 ip = st.text_input('Enter NEWS TITLE:') #creates a text box in web app
 op = model_nb.predict([ip])
